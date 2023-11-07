67e6bfff63e280a002148e902cf67c6326180431
nimj <nimj@itu.dk>
2023-11-07 13:07:14 +0100

    Add solution for problem "None"

--- redscare.py
+++ redscare.py
@@ -1,13 +1,15 @@
-red_vertices = set()
 from itu.algs4.graphs.graph import *
 from itu.algs4.graphs.digraph import *
 from itu.algs4.graphs.cycle import *
 from itu.algs4.graphs.directed_cycle import *
 from itu.algs4.graphs.breadth_first_paths import *
+
+
 n, m, r = (int(x) for x in input().split())
 s, t = input().split()
 vertices = []
 indexes = {}
+red_vertices = set()
 edges = []
 is_directed = False
 
@@ -24,3 +26,21 @@ for _ in range(m):
     edges.append((u,v))
     if edge[-1] == '>':
         is_directed = True
+
+def problem_none(V, E, R, s, t):
+    graph = Digraph(n)
+    
+    for u,v in E:
+        if v not in R or v == t:
+            graph.add_edge(indexes[u], indexes[v])
+        if not is_directed and (u not in R or u == t):
+            graph.add_edge(indexes[v], indexes[u])
+    
+    path = BreadthFirstPaths(graph, indexes[s])
+    if path.has_path_to(indexes[t]):
+        return path.dist_to(indexes[t])
+    return -1
+
+
+
+
